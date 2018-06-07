4f270075daf9ebdca96433bef46c77a632370544
Stefan Weber <chstef@outlook.com>
2018-06-07 20:29:44 +0200

    ss

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             Class1 Classtest = new Class1(",;");
             int it =  Classtest.AddCalc("[1;-2,3,4,5,6,7,8,9]");
             Console.WriteLine("Hello World!: "+ it);
